205ec5c5772952f48549416112416f1541ee8ce4
Livesayamy <90655297+Livesayamy@users.noreply.github.com>
2022-06-09 19:43:28 -0500

    Added Materials and Materials Module

---
+++ gameFrontEnd/src/app/materials.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import {MatCardActions, MatCardModule} from '@angular/material/card';
+import {MatDividerModule } from '@angular/material/divider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatListModule } from '@angular/material/list'
+
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableModule} from '@angular/material/table'
+
+
+const MatMods: any[] = [
+  MatCardModule,
+  MatDividerModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatTooltipModule,
+  MatListModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatInputModule,
+  DragDropModule,
+  MatProgressBarModule,
+  MatIconModule,
+  MatSelectModule,
+  MatPaginatorModule,
+  MatTableModule
+
+]
+
+
+@NgModule({
+  imports:
+    MatMods
+  ,
+  exports:
+    MatMods
+})
+export class MaterialsModule { }
