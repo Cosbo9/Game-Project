14f5b3ba35fbef1f6d7fe1c0a879dfa132761788
Livesayamy <90655297+Livesayamy@users.noreply.github.com>
2022-06-12 22:11:46 -0500

    Added Header, Sign-in/up components and images.
    Co-authored-by: cootsshawn <cootsshawn@users.noreply.github.com>

--- gameFrontEnd/src/app/app.module.ts
+++ gameFrontEnd/src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MaterialsModule } from './materials.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SignInComponent } from './header/sign-in/sign-in.component';
+import { SignUpComponent } from './header/sign-up/sign-up.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignInComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule,
