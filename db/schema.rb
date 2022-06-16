d696b720348303d5be623e88890905e2b814b4c2
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2022-06-16 18:51:39 -0500

    devise installed and user defined

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_16_231807) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_16_234938) do
   create_table "games", force: :cascade do |t|
     t.integer "guest_user_id"
     t.datetime "created_at", null: false
@@ -50,6 +50,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_16_231807) do
     t.integer "guest_user_id"
     t.string "token"
     t.index ["guest_user_id"], name: "index_tokens_on_guest_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "games", "guest_users", column: "hosting_user_id"
