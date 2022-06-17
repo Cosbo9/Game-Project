3937f369d5b665670453dafd6cba1f94559f53a8
Livesayamy <90655297+Livesayamy@users.noreply.github.com>
2022-06-16 19:04:28 -0500

    Cleaned up front page, moved logic into components
    Co-authored-by: Jepphire <Jepphire@users.noreply.github.com>

--- gameFrontEnd/src/app/app.module.ts
+++ gameFrontEnd/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { MaterialsModule } from './materials.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SignInComponent } from './header/sign-in/sign-in.component';
@@ -10,6 +10,7 @@ import { SignUpComponent } from './header/sign-up/sign-up.component';
 import { ProfileComponent } from './profile/profile.component';
 import { LobbyChatComponent } from './lobby-chat/lobby-chat.component';
 import { GameListComponent } from './game-list/game-list.component';
+import { GameComponent } from './game/game.component';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { GameListComponent } from './game-list/game-list.component';
     ProfileComponent,
     LobbyChatComponent,
     GameListComponent,
+    GameComponent,
   ],
   imports: [
     BrowserModule,
