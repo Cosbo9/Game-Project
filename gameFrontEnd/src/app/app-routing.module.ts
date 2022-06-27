51686cbe8078a7f06b189df8d41c9e0d5a4458e9
sirSlappz <tamestranger43@gmail.com>
2022-06-27 17:35:18 -0500

    fixed my mess

--- gameFrontEnd/src/app/app-routing.module.ts
+++ gameFrontEnd/src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GameComponent } from './game/game.component';
 import { GameplayScreenComponent } from './gameplay-screen/gameplay-screen.component';
-import { SignInComponent } from './header/sign-in/sign-in.component';
-import { SignUpComponent } from './header/sign-up/sign-up.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { LobbyComponent } from './lobby/lobby.component';
 
@@ -11,8 +9,6 @@ const routes: Routes = [
   { path: '', component: LobbyComponent },
   { path: 'game', component: GameplayScreenComponent },
   { path: 'leaderboard', component: LeaderboardComponent },
-  {path: 'sign-in', component: SignInComponent},
-  {path: 'sign-up', component: SignUpComponent}
 
 ];
 
